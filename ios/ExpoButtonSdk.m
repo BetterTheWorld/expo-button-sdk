17a4455db5ca350fbaecb6c58ecf6d1bc13b1321
ddiazFG <ddiaz@flipgive.com>
2024-11-13 15:06:42 -0500

    feat: update android JVM version and update bridge header for IOS

---
+++ ios/ExpoButtonSdk.m
@@ -0,0 +1,10 @@
+#import <ExpoModulesCore/ExpoModulesCore.h>
+
+@interface ExpoButtonSdk : NSObject <RCTBridgeModule>
+@end
+
+@implementation ExpoButtonSdk
+
+RCT_EXPORT_MODULE(ExpoButtonSdk);
+
+@end
